a9558e24fffed39c29ea0efdb05892f718038177
Sergeeva <sofiasser04@yandex.ru>
2026-06-29 00:29:41 +0300

    Изменение структуры проекта

--- src/sorted6/sorted6.cpp
+++ src/sorted6/sorted6.cpp
@@ -1,6 +1,5 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <windows.h>
 
 int partition(int arr[], int first, int last) {
     int p = arr[last];
@@ -47,42 +46,39 @@ void print_arr(int arr[], int size, const char* message) {
 
 void test_sort(int arr[], int size, const char* testName) {
     printf("\n%s\n", testName);
-    print_arr(arr, size, "  Исходный:        ");
+    print_arr(arr, size, "  Original:        ");
 
     a_rr(arr, size);
 
-    print_arr(arr, size, "  Отсортированный: ");
+    print_arr(arr, size, "  Sorted:          ");
 }
 
 int main() {
 
-    SetConsoleOutputCP(1251);
-    SetConsoleCP(1251);
-
     int arr1[] = { 5, 5, 5, 5, 5, 5, 5, 5, 5, 5 };
     int size1 = sizeof(arr1) / sizeof(arr1[0]);
-    test_sort(arr1, size1, "Массив с одинаковыми значениями");
+    test_sort(arr1, size1, "Array with identical values");
 
 
     int arr2[] = { -5, 3, -8, 1, -2, 7, -4, 6, -1, 10 };
     int size2 = sizeof(arr2) / sizeof(arr2[0]);
-    test_sort(arr2, size2, "Массив с отрицательными числами");
+    test_sort(arr2, size2, "Array with negative numbers");
 
     int arr3[] = { 42 };
     int size3 = sizeof(arr3) / sizeof(arr3[0]);
-    test_sort(arr3, size3, "Массив из одного элемента");
+    test_sort(arr3, size3, "Single element array");
 
     int arr4[] = { 64, 34, 25, 12, 22, 11, 90 };
     int size4 = sizeof(arr4) / sizeof(arr4[0]);
-    test_sort(arr4, size4, "Обычный массив");
+    test_sort(arr4, size4, "Regular array");
 
     int arr5[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
     int size5 = sizeof(arr5) / sizeof(arr5[0]);
-    test_sort(arr5, size5, "Уже отсортированный массив");
+    test_sort(arr5, size5, "Already sorted array");
 
     int arr6[] = { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
     int size6 = sizeof(arr6) / sizeof(arr6[0]);
-    test_sort(arr6, size6, "Массив в обратном порядке");
+    test_sort(arr6, size6, "Reverse sorted array");
 
     return 0;
 }
